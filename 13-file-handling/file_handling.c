9b094637c220016843955ac14ea675f2daaa4bf2
Mohammad Awez <mohammadawezansari@gmail.com>
2026-08-07 22:00:46 +0530

    Update file_handling.c

--- 13-file-handling/file_handling.c
+++ 13-file-handling/file_handling.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main() {
+    // Writing to a file
+    FILE *fptr = fopen("data.txt", "w");
+
+    if (fptr == NULL) {
+        printf("Could not open file\n");
+        return 1;
+    }
+
+    fprintf(fptr, "Hello, file!\n");
+    fclose(fptr);
+
+    // Reading from the file
+    fptr = fopen("data.txt", "r");
+    char line[100];
+    fgets(line, 100, fptr);
+    printf("Read from file: %s", line);
+    fclose(fptr);
+
+    return 0;
+}
